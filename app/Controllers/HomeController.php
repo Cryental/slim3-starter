9eb799ba78e5b77c4a43a4f406977f058d45ecbb
I'm Not Cry <44664655+Cryental@users.noreply.github.com>
2021-04-06 22:25:11 +0600

    Update HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controllers;
 
-use App\Models\AnimeEntities;
-use App\Models\AnimeMetadata;
-
 class HomeController extends BaseController
 {
     public function Index($request, $response)
